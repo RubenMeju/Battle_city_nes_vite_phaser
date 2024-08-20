73d9cd95213105c6be1c72d2b41fbba25e9abb6c
Rubén Y <rubenmeju@outlook.es>
2024-08-20 12:59:06 +0200

    fix creacion de enemigos

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -11,7 +11,7 @@ export class GameScene extends Phaser.Scene {
 
     this.escalado = 3;
     this.maxBombas = 3;
-    this.maxEnemies = 2;
+    this.maxEnemies = 3;
   }
   preload() {
     this.load.spritesheet("tiles", "assets/sprites1.png", {
@@ -36,6 +36,34 @@ export class GameScene extends Phaser.Scene {
 
     // const tileset = this.mapa.addTilesetImage("tileSets");
 
+    // Crear un grupo de enemigos
+    this.enemies = this.add.group({
+      classType: Enemy,
+      key: "enemy",
+      max: this.maxEnemies,
+      runChildUpdate: true,
+    });
+    // Eliminar los enemigos
+    this.enemies.clear(true, true);
+
+    const enemyPositions = [
+      { x: 50, y: 50 },
+      { x: this.scale.width / 2, y: 50 },
+      { x: this.scale.width - 50, y: 50 },
+    ];
+
+    if (this.maxEnemies > 0) {
+      for (let i = 0; i < this.maxEnemies; i++) {
+        const position = enemyPositions[i]; // Posición específica
+        const enemy = new Enemy(this, position.x, position.y, "enemy");
+        this.enemies.add(enemy); // Añadir al grupo
+        this.physics.add.collider(enemy, this.bloques.solidos);
+      }
+    }
+
+    // Configuración de las colisiones entre enemigos y otros objetos
+    this.physics.add.collider(this.enemies, this.bloques.solidos);
+
     // Crear el jugador
     this.jugador = new Player(this, 333, 333, "tiles", 0);
     this.physics.add.collider(this.jugador, this.bloques.solidos);
@@ -48,30 +76,6 @@ export class GameScene extends Phaser.Scene {
       null,
       this
     );
-
-    // Crear un grupo de enemigos
-    this.enemies = this.add.group({
-      classType: Enemy,
-      key: "enemy",
-      max: this.maxEnemies,
-      runChildUpdate: true,
-    });
-
-    // Rellenar el grupo con enemigos
-    for (let i = 0; i < this.maxEnemies; i++) {
-      // Calcula posiciones aleatorias o utiliza una posición predefinida
-      const x = Phaser.Math.Between(100, 700);
-      const y = Phaser.Math.Between(100, 500);
-      const enemy = this.enemies.get(x, y, "enemy");
-
-      if (enemy) {
-        //  enemy.setScale(1); // Ajusta el tamaño si es necesario
-        this.physics.add.collider(enemy, this.bloques.solidos);
-      }
-    }
-
-    // Configuración de las colisiones entre enemigos y otros objetos
-    this.physics.add.collider(this.enemies, this.bloques.solidos);
 
     //Colision balas jugador con enemigos
     this.physics.add.collider(
@@ -116,13 +120,11 @@ export class GameScene extends Phaser.Scene {
       bullet.setVisible(false);
       bullet.destroy();
 
-      enemy.anims.pause();
       enemy.anims.play("destruccion", true);
 
       // Elimina el enemigo después de la animación de destrucción
       enemy.once("animationcomplete-destruccion", () => {
-        console.log("Animación de destrucción completada");
-
+        //  console.log("Animación de destrucción completada");
         enemy.destroy();
       });
     }
