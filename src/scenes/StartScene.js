9fcc84b32a2a9bca1d688ea1dc1d4987f818c503
Rubén Y <rubenmeju@outlook.es>
2024-08-23 13:42:05 +0200

    animacion de start

--- src/scenes/StartScene.js
+++ src/scenes/StartScene.js
@@ -6,16 +6,28 @@ export class StartScene extends Phaser.Scene {
   }
 
   preload() {
-    // Aquí puedes cargar imágenes o sonidos necesarios para la pantalla de inicio
-    this.load.image("titleImage", "assets/startgame.png"); // Carga una imagen de título
+    // Cargar la imagen de título
+    this.load.image("titleImage", "assets/startgame.png");
   }
 
   create() {
-    // Mostrar el título del juego
-    this.add
-      .image(this.scale.width / 2, this.scale.height / 2 - 50, "titleImage")
-      .setScale(2)
+    // Crear la imagen en una posición inicial fuera de la pantalla
+    const titleImage = this.add
+      .image(
+        this.scale.width / 2,
+        this.scale.height + 100, // Posición inicial fuera de la pantalla
+        "titleImage",
+      )
+      .setScale(2.8)
       .setOrigin(0.5);
+
+    // Animación para mover la imagen hacia arriba
+    this.tweens.add({
+      targets: titleImage,
+      y: this.scale.height / 2 - 50, // Posición final de la imagen
+      duration: 3000, // Duración en milisegundos
+      ease: "Sine", // Tipo de suavizado de la animación
+    });
 
     // Añadir una tecla de entrada para iniciar el juego
     this.input.keyboard.once("keydown-ENTER", () => {
